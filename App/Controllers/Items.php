510a23c55b96cddf211a95d12193174ce6439142
Paweł Bochacik <pawel.bochacik.programista@gmail.com>
2023-09-21 11:13:00 +0200

    Redirected back to originally requested page after login

--- App/Controllers/Items.php
+++ App/Controllers/Items.php
@@ -18,6 +18,8 @@ class Items extends \Core\Controller {
      */
     public function indexAction() {
         if (! Auth::isLoggedIn()) {
+            Auth::rememberRequestedPage();
+            
             $this->redirect('/login');
         }
         View::renderTemplate('Items/index.html');
