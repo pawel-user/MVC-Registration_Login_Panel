5964c84555a33cc8ab97412ccae848082eb3fefd
Paweł Bochacik <pawel.bochacik.programista@gmail.com>
2023-09-21 10:25:00 +0200

    Restricted page to logged-in users only

---
+++ App/Controllers/Items.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Auth;
+
+/**
+ * Items controller (example)
+ * 
+ * PHP version 7.4
+ */
+class Items extends \Core\Controller {
+    /**
+     * Items index
+     * 
+     * @return void
+     */
+    public function indexAction() {
+        if (! Auth::isLoggedIn()) {
+            $this->redirect('/login');
+        }
+        View::renderTemplate('Items/index.html');
+    }
+}
