55d1a8f06bb5fa4309295455c62610ab5bc14c78
Paweł Bochacik <pawel.bochacik.programista@gmail.com>
2023-09-22 11:06:03 +0200

    Added acton filter to controller for require login for all action methods in controller

--- App/Controllers/Items.php
+++ App/Controllers/Items.php
@@ -12,13 +12,36 @@ use \App\Auth;
  */
 class Items extends \Core\Controller {
     /**
+     * Require the user to be authenticated vefore giving access to all methods in the controller
+     * 
+     * @return void
+     */
+    protected function before() {
+        $this->requireLogin();
+    }
+
+    /**
      * Items index
      * 
      * @return void
      */
     public function indexAction() {
-        $this->requireLogin();
-
         View::renderTemplate('Items/index.html');
+    }
+
+    /**
+     * Add a new item
+     * 
+     * @return void
+     */
+    public function newAction() {
+        echo "new action";
+    }
+
+    /**
+     * Show an item
+     */
+    public function showAction() {
+        echo "show action";
     }
 }
