34a29e01cb256f0c0b42ae32a3890532c04783b9
Paweł Bochacik <pawel.bochacik.programista@gmail.com>
2023-09-24 19:18:19 +0200

    Added method to get current authenticated user in controllers and views

--- App/Auth.php
+++ App/Auth.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use \App\Models\User;
+
 /**
  * Authentication
  * 
@@ -77,5 +79,16 @@ class Auth
      */
     public static function getReturnToPage() {
         return $_SESSION['return_to'] ?? '/';
+    }
+
+    /**
+     * Get the current logged-in user, from the session or the remember-me cookie
+     * 
+     * @return mixed The user model or null if not logged in
+     */
+    public static function getUser() {
+        if (isset($_SESSION['user_id'])) {
+            return User::findByID($_SESSION['user_id']);
+        }
     }
 }
