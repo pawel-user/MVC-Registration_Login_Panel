17fa0511647e0816a3496269cf9740ac502ed673
Pawel Bochacik <pawel.bochacik.programista@gmail.com>
2023-10-13 23:12:24 +0200

    Got email content from view template

--- Core/View.php
+++ Core/View.php
@@ -43,6 +43,21 @@ class View {
 
     public static function renderTemplate($template, $args = []) {
 
+        echo static::getTemplate($template, $args);
+    }
+
+    /**
+     * Get the contents of a view template using Twig
+     *
+     * @param string $template  The template file
+     * @param array $args  Associative array of data to display in the view (optional)
+     *
+     * @return void
+     */
+
+
+     public static function getTemplate($template, $args = []) {
+
         static $twig = null;
 
         if ($twig === null) {
@@ -52,6 +67,6 @@ class View {
             $twig->addGlobal('flash_messages', \App\Flash::getMessages());
         }
 
-        echo $twig->render($template, $args);
+        return $twig->render($template, $args);
     }
 }
